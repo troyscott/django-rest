e25751465049075f2e301098d56d44ae4a38b577
Troy Scott <troys2005@gmail.com>
2016-01-30 23:48:57 -0800

    Lightweight Django - Ch. 4
    Setup the Django Restful Framework.  Backbone can now be integrated with backend.

---
+++ scrum/board/serializers.py
@@ -0,0 +1,70 @@
+from django.contrib.auth import get_user_model
+
+from rest_framework import serializers
+from rest_framework import reverse
+
+from .models import Sprint, Task
+
+User = get_user_model()
+
+class SprintSerializer(serializers.ModelSerializer):
+
+	links = serializers.SerializerMethodField()
+
+	class Meta:
+		model = Sprint
+		fields = ('id', 'name', 'description', 'end','links', )
+
+	def get_links(self, obj):
+		request = self.context['request']
+		return {
+			'self': reverse('sprint-detail', kwargs={'pk': obj.pk}, request=request),
+			}
+
+
+		
+class TaskSerializer(serializers.ModelSerializer):
+
+	assigned = 'assigned'
+	#serializers.SlugRelatedField(
+	#		slug_field=User.USERNAME_FIELD, required=False)
+	status_display = serializers.SerializerMethodField()
+	links = serializers.SerializerMethodField()
+
+
+	class Meta:
+		model = Task
+		fields = ('id', 'name', 'description', 'sprint', 'status', 
+				'status_display', 'order', 'assigned', 'started', 
+				'due', 'completed', 'links')
+
+	def get_status_display(self, obj):
+		return obj.get_status_display()
+
+
+	def get_links(self, obj):
+		request = self.context['request']
+		return {
+			'self': reverse('task-detail', kwargs={'pk': obj.pk}, request=request),
+		}
+
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+	full_name = serializers.CharField(source='get_full_name', read_only=True)
+	links = serializers.SerializerMethodField()
+
+	class Meta:
+		model = User
+		fields = ('id', User.USERNAME_FIELD, 'full_name', 'is_active',)
+
+	def get_links(self, obj):
+		request = self.context['request']
+		return {
+				'self': reverse('user-detail', kwargs={User.USERNAME_FIELD: username}, 
+					request=request),
+		}
+
+
