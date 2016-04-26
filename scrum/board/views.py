b248f71cd9e4c158497650c708f54ca01ae168f8
Troy Scott <troys2005@gmail.com>
2016-04-25 19:15:02 -0700

    Add filters

--- scrum/board/views.py
+++ scrum/board/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
-from rest_framework import authentication, permissions, viewsets
+from rest_framework import authentication, permissions, viewsets, filters
 
+from .forms import TaskFilter, SprintFilter
 from .models import Sprint, Task
 from .serializers import SprintSerializer, TaskSerializer, UserSerializer
 
@@ -22,11 +23,21 @@ class DefaultMixin(object):
 	paginate_by_param = 'page_size'
 	max_paginate_by = 100
 
+	filter_backends = (
+		filters.DjangoFilterBackend,
+		filters.SearchFilter,
+		filters.OrderingFilter,
+
+	)
 
 class SprintViewSet(viewsets.ModelViewSet):
 	"""API endpoint for listing and creating sprints"""
 	queryset = Sprint.objects.order_by('end')
 	serializer_class = SprintSerializer
+	filter_class = SprintFilter
+	search_fields = ('name',)
+	ordering_fields = ('end', 'name',)
+
 
 
 class TaskViewSet(DefaultMixin, viewsets.ModelViewSet):
@@ -34,6 +45,10 @@ class TaskViewSet(DefaultMixin, viewsets.ModelViewSet):
 
 	queryset = Task.objects.all()
 	serializer_class = TaskSerializer
+	filter_class = TaskFilter
+	search_fields = ('name', 'description',)
+	ordering_fields = ('name', 'order', 'started', 'due', 'completed',)
+
 
 class UserViewSet(DefaultMixin, viewsets.ReadOnlyModelViewSet):
 	"""API endpoint for listing users"""
@@ -42,5 +57,7 @@ class UserViewSet(DefaultMixin, viewsets.ReadOnlyModelViewSet):
 	lookup_url_kwarg = User.USERNAME_FIELD
 	queryset = User.objects.order_by(User.USERNAME_FIELD)
 	serializer_class = UserSerializer
+	search_fields = (User.USERNAME_FIELD, )
+
 
 
